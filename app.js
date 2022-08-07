f132ad679bdd59c2bad879d11a662d507d3117a5
AgoroAkorede <agoroakorede@gmail.com>
2022-08-07 17:51:36 +0100

    8/7/22

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
-Vue.createApp({
+const vm = Vue.createApp({
+  data() {
+    return {
+      firstName: "Akorede",
+      middleName: "",
+      lastName: "Agoro",
+      url: "https://google.com",
+      raw_url: '<a href="url" target="_blank">Google</a>',
+      age: 20,
+    };
+  },
+  methods: {
+    increment() {
+      this.age++;
+    },
+    updateLastName(msg, event) {
+      this.lastName = event.target.value;
+      console.log(msg);
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+      },
+      
+    },
+  
+    watch: {
+        age(newVal,oldVal) {
+            setTimeout(() => {
+                 this.age=20
+             },3000)
+      }
+  }
+}).mount("#app");
 
-}).mount("#app")
+// setTimeout(() => {
+//     vm.firstName="Emmanuel"
+// },2000)
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName: "Damilare",
+//             lastName:"Agoro"
+//         }
+//     }
+// }).mount("#app2")
