b765e22361d2da92875c2f12032ff061e386f1a6
AgoroAkorede <agoroakorede@gmail.com>
2022-08-09 21:49:01 +0100

    feat: created google cli

---
+++ section-7-1-STARTER/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Hey!</p>
+    <Hello />
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello";
+export default { 
+  name: "App",
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style></style>
---
+++ section-7-1-STARTER/src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>
+    {{ msg }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  data() {
+    return {
+      msg: "hello world!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+    p{
+        color:red;
+    }
+</style>
